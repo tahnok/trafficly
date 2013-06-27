398bba27f347157548d98589be656dc6ad2b6b47
Wesley Ellis <tahnok@gmail.com>
2013-06-27 17:32:54 -0400

    make it daemonized

---
+++ daemon.rb
@@ -0,0 +1,10 @@
+require 'daemons'
+
+`echo "4" > /sys/class/gpio/export`
+`echo "out" > /sys/class/gpio/gpio4/direction`
+`echo "17" > /sys/class/gpio/export`
+`echo "out" > /sys/class/gpio/gpio17/direction`
+`echo "18" > /sys/class/gpio/export`
+`echo "out" > /sys/class/gpio/gpio18/direction`
+
+Daemons.run('/home/pi/trafficly/main.rb')
